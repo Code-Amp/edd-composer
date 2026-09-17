6e6e0cdba3ab6c587e7bf7ccaa9986fd15647c5a
Ross Morsali <ross@codeamp.io>
2026-09-17 12:20:01 +0200

    Fix Composer admin modal and table overflow

--- edd-composer/assets/index.asset.php
+++ edd-composer/assets/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-date', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-notices'), 'version' => '5c9fe58e691fb7739848');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-date', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-notices'), 'version' => '794dd7cbdd92e929cf94');
